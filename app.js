7ec7fb6accb5a9d423154d1060786448e6121bb0
Tsao <tsaohucn@gmail.com>
2017-06-12 23:57:49 +0800

    config babel/eslint/nodemon

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var app = express();
+"use strict"
+import express from 'express'
 
+const app = express()
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
-  res.send('hello world');
+  res.send('hello world')
 })
 
 app.listen(3000, function () {
