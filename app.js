4d6fa52abf41c2966f743c564943948025db5520
Tsao <tsaohucn@gmail.com>
2017-06-14 17:29:03 +0800

    dynamic heroku port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/articles/new', function(req, res) {
   res.status(200).json({ name: 'tobi' });
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
 })
 
