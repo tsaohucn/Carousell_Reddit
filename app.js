e25fdd382e222bf2b88683968b84471b596fb9ef
Tsao <tsaohucn@gmail.com>
2017-06-20 18:53:14 +0800

    complete protype

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 "use strict"
 import path from 'path'
 import express from 'express'
+import bodyParser from 'body-parser'
 
 const app = express()
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+var articles = new Array
+//routes
 app.get('/', function(req, res) {
   res.render('index')
 })
 
+app.get('/articles', function(req, res) {
+  res.render('articles/index',{ articles: articles })
+})
+
+app.get('/articles/new', function(req, res) {
+  res.render('articles/new')
+})
+
+app.post('/articles', function(req, res) {
+  articles.push(req.body)
+  res.render('articles/index',{ articles: articles })
+})
+
+//server
 app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
 })
