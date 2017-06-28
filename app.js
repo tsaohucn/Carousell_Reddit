9bc64e28d33c9edac3a42215e25b18eb03382341
Tsao <tsaohucn@gmail.com>
2017-06-28 18:28:17 +0800

    fix render to redirect

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ import dateTime from 'node-datetime'
 import xssFilters from 'xss-filters'
 import csurf from 'csurf'
 
-// app config
+// ------App config------
 const app = express()
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -25,45 +25,52 @@ app.use(session({
 }))
 app.use(flash())
 
-// #### Save articles in memory ####
+// ------Save articles in memory------
 var articles = new Array
 
-// #### routes ####
-// #### http get ####
-// #### Home page to show top 20 articles sort by votes ####
+// ------Routes------
+// ------Http get------
+// Get /
+// #### home page to show top 20 articles sort by votes ####
 app.get('/', function(req, res) {
   const articles_top_20 = articles.slice().sort(function(a,b){return b.votes-a.votes}).slice(0,20)
   res.render('index',{ articles: articles_top_20 })
 })
-// #### Articles to show all articles ####
+// Get /articles
+// #### articles page to show all articles ####
 app.get('/articles', function(req, res) {
   res.render('articles/index',{ articles: articles })
 })
-// #### Article new page to show form let user can create new article ####
+// Get /articles/new
+// #### article new page to show form let user can create new article ####
 app.get('/articles/new', function(req, res) {
   res.render('articles/new',{ error: null })
 })
-// #### http post ####
-// #### publish new article and save in global variable ####
-// #### charts need correspond limit or return Article new page ####
+
+// ------Http post------
+// Post /articles
+// #### title and content need correspond limit or return /articles/new.ejs ####
+// #### if correspond limit then create new article and save in global variable ####
 app.post('/articles', function(req, res) {
-  // #### if title no smaller than 20 or content no smaller than 255 then return Article new page ####
+  // #### if title no smaller than 20 or content no smaller than 255 then render /articles/new.ejs ####
   if (req.body.content.length > 255 || req.body.title.length > 20) {
     req.flash('error',"Post Fail : Your content can't over 255 charts and title can't over 20 charts")
     res.render('articles/new',{ error: req.flash('error') })
   } else {
   // #### if title and content correspond limit then create new article and give it uuid as well as postTime ####
+  // #### then push to articles variable then redirect to Get /articles ####
     const title = req.body.title
     const content = req.body.content
     const id = uuidv4()
     const postTime = dateTime.create().format('Y-m-d H:M:S')
     const article = { id: id, title: title, content: content, votes: 0, postTime: postTime }
     articles.push(article)
-    res.render('articles/index',{ articles: articles })
+    res.redirect('/articles')
   }
 })
 
-// #### Ajax API for thumbsUp and thumbsDown ####
+// ------Ajax API------
+// #### for thumbsUp and thumbsDown ####
 // #### return votes to let view page to refresh votes count ####
 app.post('/thumbsUp', function(req, res) {
   let article = articles.find(ele => ele.id === req.body.uuid)
@@ -80,7 +87,7 @@ app.post('/thumbsDown', function(req, res) {
   res.send(String(article.votes))
 })
 
-// server
+// ------Server------
 var server = app.listen(app.get('port'), function () {
   console.log('Example app listening on port 3000!')
 })
