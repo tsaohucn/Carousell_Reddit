82dcc4e5f158ca7e21643af13b2fd3d407ad6d81
Tsao <tsaohucn@gmail.com>
2017-06-28 18:13:54 +0800

    enhanced code comments

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.get('/', function(req, res) {
   const articles_top_20 = articles.slice().sort(function(a,b){return b.votes-a.votes}).slice(0,20)
   res.render('index',{ articles: articles_top_20 })
 })
-// #### Articles to show all articles####
+// #### Articles to show all articles ####
 app.get('/articles', function(req, res) {
   res.render('articles/index',{ articles: articles })
 })
@@ -44,13 +44,15 @@ app.get('/articles/new', function(req, res) {
   res.render('articles/new',{ error: null })
 })
 // #### http post ####
-// #### publish new article and save in global variable####
-// #### charts need correspond limit or return Article new page####
+// #### publish new article and save in global variable ####
+// #### charts need correspond limit or return Article new page ####
 app.post('/articles', function(req, res) {
+  // #### if title no smaller than 20 or content no smaller than 255 then return Article new page ####
   if (req.body.content.length > 255 || req.body.title.length > 20) {
     req.flash('error',"Post Fail : Your content can't over 255 charts and title can't over 20 charts")
     res.render('articles/new',{ error: req.flash('error') })
   } else {
+  // #### if title and content correspond limit then create new article and give it uuid as well as postTime ####
     const title = req.body.title
     const content = req.body.content
     const id = uuidv4()
@@ -61,8 +63,8 @@ app.post('/articles', function(req, res) {
   }
 })
 
-// #### Ajax API for thumbsUp and thumbsDown####
-// #### return votes to let view page to refresh votes count####
+// #### Ajax API for thumbsUp and thumbsDown ####
+// #### return votes to let view page to refresh votes count ####
 app.post('/thumbsUp', function(req, res) {
   let article = articles.find(ele => ele.id === req.body.uuid)
   article.votes ++
@@ -71,6 +73,7 @@ app.post('/thumbsUp', function(req, res) {
 
 app.post('/thumbsDown', function(req, res) {
   let article = articles.find(ele => ele.id === req.body.uuid)
+  // #### votest min must be zero ####
   if (article.votes > 0) {
     article.votes --
   }
